0a9567f1e61be24a47ef2554bf46d2a4b88cdbed
hack4day <phamminhhieu@prati.com.vn>
2022-03-29 15:44:42 +0700

    Switch to template

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the landing page")
+    # return HttpResponse("Welcome to the landing page")
+    return render(request, 'landing/index.html')
